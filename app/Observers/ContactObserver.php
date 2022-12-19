b0099550cca32932eed75c6eae34413cedbb8f66
Miguel Portero <migporuiz@gmail.com>
2022-12-19 09:35:24 +0100

    first commit

---
+++ app/Observers/ContactObserver.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Observers;
+
+use App\Mail\ContactMail;
+use App\Models\Contact;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
+
+class ContactObserver
+{
+    /**
+     * Handle events after all transactions are committed.
+     *
+     * @var bool
+     */
+    public $afterCommit = true;
+    
+    public function created(Contact $contact)
+    {
+
+        try {
+            //Log::info('contacto:', ['contact'=>$contact]);
+            $mailContact = new ContactMail($contact);
+            //Default email
+            Mail::to('migporuiz@gmail.com')->send($mailContact);
+            Log::info('Email sended ok');
+        } catch (\Exception $e) {
+            Log::error('Error sending email', $e);
+        }
+    }
+
+    /**
+     * Handle the Contact "updated" event.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return void
+     */
+    public function updated(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Handle the Contact "deleted" event.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return void
+     */
+    public function deleted(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Handle the Contact "restored" event.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return void
+     */
+    public function restored(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Handle the Contact "force deleted" event.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return void
+     */
+    public function forceDeleted(Contact $contact)
+    {
+        //
+    }
+}
