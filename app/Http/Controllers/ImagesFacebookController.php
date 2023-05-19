c56b86563c01881da38cc2ac49c25fa24c7f3aee
Miguel <migporuiz@gmail.com>
2023-05-19 14:12:51 +0200

    ImagesFacebookController explanation

--- app/Http/Controllers/ImagesFacebookController.php
+++ app/Http/Controllers/ImagesFacebookController.php
@@ -8,42 +8,51 @@ use Illuminate\Support\Facades\Storage;
 
 class ImagesFacebookController extends Controller
 {
-  public function store(Request $request)
-  {
-      // Verifies if the image file is present in the request
-    if ($request->hasFile('image')) {
+    public function store(Request $request)
+    {
+        // Check if an image file has been sent in the request
+        // hasFile('') es un método de la clase 'Request'  que verifica si existe un archivo con el nombre
+        // de entrada especificado en la solicitud
+        if ($request->hasFile('image')) {
+            // $request->file('key') --> Obtiene un objeto UploadedFile para un archivo cargado en la solicitud.
+            // Store the image in the "public/images" folder and save the path in the application
+            $image = $request->file('image')->store('public/images');
+            //Añade la ruta a la variable $image
+            $request->merge(['route' => $image]);
 
-      // Stores the image in the "public/images" folder and saves the path in the application
-      $image = $request->file('image')->store('public/images');
-      $request->request->add(['route' => $image]);
+            $sizeInKB = $request->file('image')->getSize();
 
-      $sizeInKB = $request->file('image')->getSize();
+            // Create a new "ImagesFacebook" object and fill it with the request data
+            $image = ImagesFacebook::create([
+                'name' => $request->file('image')->getClientOriginalName(),
+                'route' => $request->input('route'),
+                'size' => $sizeInKB, //$request->file('image')->getSize(); --> por qué no se pone esto directamente?
+                'facebook_posts_id' => $request->input('facebook_posts_id'),
+                'type' => $request->file('image')->getClientOriginalExtension(),
+            ]);
 
-      // Creates a new "ImagesFacebook" object and fills it with the request data
-      $image = ImagesFacebook::create([
-        'name' => $request->file('image')->getClientOriginalName(),
-        'route' => $request->route,
-        'size' => $sizeInKB,
-        'facebook_posts_id' => $request->input('facebook_posts_id'),
-        'type' => $request->file('image')->getClientOriginalExtension(),
-      ]);
+            // Return a JSON response containing the created "image" object
+            return response()->json(['image' => $image]);
+        }
 
-      // Returns a JSON response containing the created "image" object
-      return response()->json(['image' => $image]);
+        // If no image file is found in the request, return a JSON response indicating that no image was uploaded.
+        return response()->json(['message' => 'No image was uploaded']);
     }
-    // If no image file is found in the request, returns a JSON response indicating that no image was uploaded.
-    return response()->json(['message' => 'No image was uploaded']);
-  }
 
-  public function getAll(Request $request) {
-    $images = ImagesFacebook::all()->map(function ($image) {
-        $url = asset(Storage::url($image->route));
-        $image->url = $url;
-        unset($image->route);
-        return $image;
-    });
-    
-    return response()->json($images);
-  }
+    public function getAll(Request $request)
+    {
+        $images = ImagesFacebook::all()->map(function ($image) {
+            // Obtiene la URL pública completa para el archivo de imagen almacenado en la propiedad "$image->route"
+            // asset() genera la URL base de la app
+            // Storage::url() obtiene la ruta del archivo dentro del sistema de almacenamiento de laravel
+            $url = asset(Storage::url($image->route));
+            // Asigna la URL obtenida anteriormente a una nueva propiedad llamada url en el objeto $image.
+            $image->url = $url;
+            // Elimina la propiedad "$image->route" del objeto $image, ya que no se necesita más después de obtener la URL.
+            unset($image->route);
+            return $image;
+        });
 
+        return response()->json($images);
+    }
 }
